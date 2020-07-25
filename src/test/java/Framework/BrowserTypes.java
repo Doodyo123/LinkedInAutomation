fa6f1c0f613c42bdd7f609ea740fa9701c03cd41
Derick <Derickho94@yahoo.com>
2020-07-24 18:18:22 -0700

    created a new project and copied automation practice framework

---
+++ src/test/java/Framework/BrowserTypes.java
@@ -0,0 +1,8 @@
+package Framework;
+
+public class BrowserTypes {
+
+	public static final String Chrome = "chrome";
+	public static final String Firefox = "firefox";
+	public static final String Edge = "edge";
+}
---
+++ src/test/java/Framework/DriverManagerFactory.java
@@ -0,0 +1,20 @@
+package Framework;
+
+public class DriverManagerFactory {
+	
+	public static DriverManager getManager(String  browserType) {
+	
+		if(browserType.equals(BrowserTypes.Chrome)) {
+			return new ChromeDriverManager();
+		}
+		if(browserType.equals(BrowserTypes.Firefox)) {
+			System.out.println("firefox driver initiating");
+			return new FirefoxDriverManager();
+		}
+		if(browserType.equals(BrowserTypes.Edge)) {
+			return new MSEdgeDriverManager();
+		}
+			
+		throw new RuntimeException(browserType + " is not a supported browser.");
+	}
+}
